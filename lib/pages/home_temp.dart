92e18e3eaf652d815f4dc84be261a0212391f1cb
mari <mari.17ymy@gmail.com>
2020-04-10 01:04:57 -0400

    scroll, lista

---
+++ lib/pages/home_temp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(title: Text('Listaaaaaa')),
+          Divider(),
+          ListTile(title: Text('Listaaaaaa')),
+          Divider(),
+          ListTile(title: Text('Listaaaaaa')),
+
+        ],
+      ),
+
+    );
+  }
+}
