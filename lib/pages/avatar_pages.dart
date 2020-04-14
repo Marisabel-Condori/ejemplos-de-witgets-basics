1f3e3850ea7e662a97d5c4e187e5d26826f2eef0
mari <mari.17ymy@gmail.com>
2020-04-14 01:16:09 -0400

    card avatar: circleAvatar, body: fadeImage(como si la imagen estuviera cargando)

--- lib/pages/avatar_pages.dart
+++ lib/pages/avatar_pages.dart
@@ -5,8 +5,26 @@ class AvatarPages extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar'),),
+      appBar: AppBar(
+        title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/V3YUL2OH4RF4VHJ25NLBW7KGZY.jpg'),
+              radius: 26.0,
+            ),
+            padding: EdgeInsets.all(5.0),
+          ),
+          
+          Container(
+            child: CircleAvatar(child: Text('G'), backgroundColor: Colors.white,),
+            padding:EdgeInsets.all(5.0),
+            )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(onPressed: (){}),
+      body: Center(child: FadeInImage(placeholder: AssetImage('asset/jar-loading.gif'), image: NetworkImage('https://3.bp.blogspot.com/-dL7cP9aj6FU/WSOI0GHwPJI/AAAAAAAHqD0/I_yJ4kSqkzw_OIBTxiiQf5-cyRsw4cpMACLcB/s1600/groot-baby1.jpg')))
+
     ) ;
   }
 }
