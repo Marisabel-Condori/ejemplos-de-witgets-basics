26e6b42261d475260b9a7902834dafdebc4adca8
mari <mari.17ymy@gmail.com>
2020-04-14 00:12:07 -0400

    en alert: showDialog y AlertDialog

--- lib/pages/alert_pages.dart
+++ lib/pages/alert_pages.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
-class AlertPages extends StatelessWidget{
-
+class AlertPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Alert'),),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.gps_not_fixed) ,onPressed: (){Navigator.pop(context);}),
-      body: Center(child: RaisedButton(child: Text('Mensaje'), 
-                                        color: Colors.blueGrey, 
-                                        textColor: Colors.white, 
-                                        shape: StadiumBorder(),
-                                        onPressed: (){}
-                                        ),),
+      appBar: AppBar(
+        title: Text('Alert'),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.gps_not_fixed),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Mensaje'),
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+            onPressed: () => _mostrarAlert(context)),
+      ),
+    );
+  }
 
-    ) ;
+  void _mostrarAlert(BuildContext context){
+    showDialog(context: context,
+    barrierDismissible:true,
+    builder: (context){
+      return AlertDialog(
+        title: Text('tituloooo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('contenidoooooooooooooooooooooooooooooooooooooooooooooooooo'),
+            FlutterLogo(size: 100.0)
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('cancelar')),
+          FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('ok'))
+        ],
+      );
+    }
+    
+    );
   }
 }
