03b54ceeee1737f59d024ff65f3028c82be1641d
mari <mari.17ymy@gmail.com>
2020-04-14 22:35:16 -0400

    contenedor rosado en ContainerPages

--- lib/pages/animated_container.dart
+++ lib/pages/animated_container.dart
@@ -4,16 +4,30 @@ import 'package:flutter/material.dart';
 
 class AnimatedContainerPage extends StatefulWidget {
   AnimatedContainerPage({Key key}) : super(key: key);
-
   @override
   _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(10.0);
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('holaaaaaaa'),
-    );
+    return Scaffold(
+      appBar: AppBar(title: Text('Animated cContainer'),),
+      body: Center(
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            borderRadius: _borderRadius,
+            color: _color),
+        )
+      ),
+    ) ;
   }
 }
