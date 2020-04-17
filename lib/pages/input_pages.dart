ba61c37834887578b14b2506e69f6a99de0083fa
mari <mari.17ymy@gmail.com>
2020-04-17 01:42:22 -0400

    personalizando el dropdown

--- lib/pages/input_pages.dart
+++ lib/pages/input_pages.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class InputPages extends StatefulWidget {
   InputPages({Key key}) : super(key: key);
 
@@ -22,7 +21,9 @@ class _InputPagesState extends State<InputPages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text('Inputs'),),
+      appBar: AppBar(
+        title: Text('Inputs'),
+      ),
       body: ListView(
         padding: EdgeInsets.only(left: 10.0, right: 5.0, top: 10.0),
         children: <Widget>[
@@ -46,21 +47,20 @@ class _InputPagesState extends State<InputPages> {
     return TextField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
-        hintText: 'Nombre',
-        counterText: 'letras: ${_nombre.length}',
-        icon: Icon(Icons.person),
-        suffixIcon: Icon(Icons.person_pin),
-        helperText: 'esta es la ayuda',
-        labelText: 'Nombre',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0))
-        ),
-        onChanged: (valor){
-          setState(() {
-             _nombre = valor;
-          });
-            print(_nombre);
-        },
+          hintText: 'Nombre',
+          counterText: 'letras: ${_nombre.length}',
+          icon: Icon(Icons.person),
+          suffixIcon: Icon(Icons.person_pin),
+          helperText: 'esta es la ayuda',
+          labelText: 'Nombre',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+        print(_nombre);
+      },
     );
   }
 
@@ -68,19 +68,18 @@ class _InputPagesState extends State<InputPages> {
     return TextField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        hintText: 'Email',
-        icon: Icon(Icons.email),
-        suffixIcon: Icon(Icons.email),
-        labelText: 'Email',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0))
-        ),
-        onChanged: (valor){
-          setState(() {
-             _email = valor;
-          });
-            print(_email);
-        },
+          hintText: 'Email',
+          icon: Icon(Icons.email),
+          suffixIcon: Icon(Icons.email),
+          labelText: 'Email',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+        print(_email);
+      },
     );
   }
 
@@ -88,76 +87,78 @@ class _InputPagesState extends State<InputPages> {
     return TextField(
       obscureText: true,
       decoration: InputDecoration(
-        hintText: 'Password',
-        icon: Icon(Icons.lock_open),
-        suffixIcon: Icon(Icons.lock_open),
-        labelText: 'Password',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0))
-        ),
+          hintText: 'Password',
+          icon: Icon(Icons.lock_open),
+          suffixIcon: Icon(Icons.lock_open),
+          labelText: 'Password',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
     );
   }
 
-   Widget _crearFecha(context) {
+  Widget _crearFecha(context) {
     return TextField(
       controller: _modificaFecha,
       enableInteractiveSelection: false,
       decoration: InputDecoration(
-        hintText: 'Fecha',
-        icon: Icon(Icons.calendar_today),
-        suffixIcon: Icon(Icons.perm_contact_calendar),
-        labelText: 'Fecha',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0))
-        ),
-        onTap: (){
-          FocusScope.of(context).requestFocus(new FocusNode());
-          _selectDate(context);
-        },
+          hintText: 'Fecha',
+          icon: Icon(Icons.calendar_today),
+          suffixIcon: Icon(Icons.perm_contact_calendar),
+          labelText: 'Fecha',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
     );
   }
 
-  void _selectDate(BuildContext context) async{
+  void _selectDate(BuildContext context) async {
     DateTime picket = await showDatePicker(
-      context: context, 
-      initialDate: new DateTime.now(), 
-      firstDate: new DateTime(2019), 
-      lastDate: new DateTime(2022),
-      locale: Locale('es','ES')
-      );
-      if (picket != null ){
-        setState(() {
-          _fecha = picket.toString();
-          _modificaFecha.text = _fecha;
-        });
-      }
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2019),
+        lastDate: new DateTime(2022),
+        locale: Locale('es', 'ES'));
+    if (picket != null) {
+      setState(() {
+        _fecha = picket.toString();
+        _modificaFecha.text = _fecha;
+      });
+    }
   }
 
   _crearDropdown() {
-    return DropdownButton(
-      value: _opcionSeleccionada,
-      items: getOpciones(), 
-      onChanged: (opt){
-        setState(() {
-          _opcionSeleccionada = opt;
-        });
-        });
+    return Row(
+      children: <Widget>[
+        Icon(Icons.perm_scan_wifi),
+        SizedBox(width: 20.0),
+        DropdownButton(
+            value: _opcionSeleccionada,
+            items: getOpciones(),
+            onChanged: (opt) {
+              setState(() {
+                _opcionSeleccionada = opt;
+              });
+            }),
+      ],
+    );
   }
 
-  List<DropdownMenuItem<String>> getOpciones(){
+  List<DropdownMenuItem<String>> getOpciones() {
     List<DropdownMenuItem<String>> lista = new List();
-    _poderes.forEach((opt){
-      lista.add(DropdownMenuItem(child: Text(opt),value: opt));
-    }) ;
+    _poderes.forEach((opt) {
+      lista.add(DropdownMenuItem(child: Text(opt), value: opt));
+    });
     return lista;
   }
 
-  Widget _crearPersona(){
+  Widget _crearPersona() {
     return ListTile(
       title: Text('nombre de la persona es: $_nombre'),
       subtitle: Text('email: $_email'),
+      trailing: Text(_opcionSeleccionada),
     );
   }
 }
-
-  
